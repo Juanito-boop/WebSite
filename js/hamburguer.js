2a4aa1629c9d31409a9e9b2dd320e85053a6984f
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2022-05-31 10:02:54 -0500

    nuevos cambios

---
+++ js/hamburguer.js
@@ -0,0 +1,17 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+  menu = document.querySelector('#menu')
+
+iconoMenu.addEventListener('click', (e) => {
+  // Alternamos estilos para el menu y body
+  menu.classList.toggle('active')
+  document.body.classList.toggle('opacity')
+
+  // Alternamos su atributo 'src' para el ícono del menú
+  const rutaActual = e.target.getAttribute('src')
+
+  if (rutaActual == './img/icono-menu.svg') {
+    e.target.setAttribute('src', './img/icono-menu.svg')
+  } else {
+    e.target.setAttribute('src', './img/icono-menu.svg')
+  }
+})
